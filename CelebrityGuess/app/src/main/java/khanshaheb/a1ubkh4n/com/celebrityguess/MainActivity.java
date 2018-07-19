ccf54cd7ad77af879bbfb03675f62c93d2ecdd98
a1ubkh4n <aiubkhan95@gmail.com>
2018-07-20 00:57:29 +0600

    Showing Random Celebrity in Layout

--- CelebrityGuess/app/src/main/java/khanshaheb/a1ubkh4n/com/celebrityguess/MainActivity.java
+++ CelebrityGuess/app/src/main/java/khanshaheb/a1ubkh4n/com/celebrityguess/MainActivity.java
@@ -1,18 +1,62 @@
 package khanshaheb.a1ubkh4n.com.celebrityguess;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> celebURLs = new ArrayList<String>();
+    ArrayList<String> celebNames = new ArrayList<String>();
+    int choosenCeleb = 0;
+
+    ImageView imageView;
+
+
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+
+            URL url = null;
+            try {
+                url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream inputStream = connection.getInputStream();
+
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+
+                return myBitmap;
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
 
     public class DownloadTask extends AsyncTask<String, Void, String> {
         @Override
@@ -57,6 +101,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageView = (ImageView) findViewById(R.id.celebImageView);
+
         DownloadTask task = new DownloadTask();
 
         String result = null;
@@ -64,11 +110,44 @@ public class MainActivity extends AppCompatActivity {
         try {
             result = task.execute("http://www.posh24.se/kandisar").get();
 
-            Log.i("Contents of URL:", result);
+            String[] splitResult = result.split("<div class=\"sidebarContainer\">");
+
+            Pattern p = Pattern.compile("img src=\"(.*?)\"");
+            Matcher m = p.matcher(splitResult[0]);
+
+            while (m.find()) {
+                celebURLs.add(m.group(1));
+
+            }
+
+            p = Pattern.compile("alt=\"(.*?)\"");
+            m = p.matcher(splitResult[0]);
+
+            while (m.find()) {
+
+                celebNames.add(m.group(1));
+
+            }
+
+            Random random = new Random();
+            choosenCeleb = random.nextInt(celebURLs.size());
+
+
+            ImageDownloader imageTask = new ImageDownloader();
+            Bitmap celebImage;
+
+            celebImage = imageTask.execute(celebURLs.get(choosenCeleb)).get();
+
+            imageView.setImageBitmap(celebImage);
+
+
 
         } catch (InterruptedException e) {
+
             e.printStackTrace();
+
         } catch (ExecutionException e) {
+
             e.printStackTrace();
         }
     }
